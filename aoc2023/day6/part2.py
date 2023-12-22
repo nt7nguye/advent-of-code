5fba8b2dd7bad96354aff98c307ec60e8cc3c168
Tan Nguyen <tan@secoda.co>
2023-12-22 15:37:34 -0500

    day 6 finished

--- aoc2023/day6/part2.py
+++ aoc2023/day6/part2.py
@@ -1,21 +1,9 @@
 
-
 def solve(input):
-    card_instances = [1] * 186
-    for i, line in enumerate(input):
-        tokens = line.split(' ')
-        winning = set()
-        checking = False
-        point = 0
-        for token in tokens[2:]:
-            if token == '':
-                continue
-            elif token == '|':
-                checking = True
-            elif checking:
-                point += token in winning
-            else:
-                winning.add(token)
-        for j in range(i + 1, min(186, i + 1 + point)):
-            card_instances[j] += card_instances[i]
-    return sum(card_instances)
+    time = int(''.join(input[0].split()[1:]))
+    distance = int(''.join(input[1].split()[1:]))
+    beat = 0
+    for i in range(time):
+        total_dist = (time - i) * i
+        beat += 1 if total_dist > distance else 0
+    return beat
