ce66ee69ce5fb3b1fe84e06838856a44ec67301b
Tan Nguyen <tan@secoda.co>
2024-12-02 08:52:50 -0500

    wip

---
+++ aoc2024/target/debug/deps/advent_of_code-607676be7ea4ad9d.d
@@ -0,0 +1,5 @@
+/Users/tannguyen/Desktop/advent-of-code/aoc2024/target/debug/deps/libadvent_of_code-607676be7ea4ad9d.rmeta: src/main.rs
+
+/Users/tannguyen/Desktop/advent-of-code/aoc2024/target/debug/deps/advent_of_code-607676be7ea4ad9d.d: src/main.rs
+
+src/main.rs:
---
+++ aoc2024/target/debug/deps/advent_of_code-8832c05a3e533390.d
@@ -0,0 +1,5 @@
+/Users/tannguyen/Desktop/advent-of-code/aoc2024/target/debug/deps/libadvent_of_code-8832c05a3e533390.rmeta: src/main.rs
+
+/Users/tannguyen/Desktop/advent-of-code/aoc2024/target/debug/deps/advent_of_code-8832c05a3e533390.d: src/main.rs
+
+src/main.rs:
